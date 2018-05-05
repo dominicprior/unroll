352c3f69b87c30dc67f58b7c9cd0699e57b915fd
Dominic Prior <dominicprior@yahoo.co.uk>
2018-05-05 19:26:04 +0100

    Add first version of the unroll parser
    Running k correctly gives " pr(0) pr(1) pr(2)".

--- unroll.hs
+++ unroll.hs
@@ -1,3 +1,146 @@
+{-# LANGUAGE FlexibleContexts #-}
+import Text.Parsec
+import Data.Map
+
+type M = Data.Map.Map String Int
+type P = Parsec String M String
+sp = spaces
+fromRight (Right x) = x
+
+tryStr :: String -> P
+tryStr = try . string
+
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+t p m = runParser p m ""
+
+k = t forLoop empty "for (i=0;i<3;i++) pr(i);"
+
+forLoop :: P
+forLoop = do
+  tryStr "for" >> sp
+  (var, a) <- header
+  b <- body
+  env <- getState
+  return $ concatMap (subst' var env b) a
+
+-- subst "i" empty "pr(i)" 3 = "pr(3)"
+subst' :: String -> M -> String -> Int -> String
+subst' var env b val = subst (insert var val env) b
+
+-- subst (fromList [("i",3)]) "i+i" = "3+3"
+subst :: M -> String -> String
+subst env str = fromRight $ runParser substParser env "" str
+
+substParser :: P
+substParser = fmap concat $ many1 substToken
+
+substToken :: P
+substToken = many1 space <|> substVar
+  <|> many1 (noneOf $
+       ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ " _\t\n")
+
+substVar :: P
+substVar = do
+  var <- many1 idChar
+  env <- getState
+  return $
+    case (Data.Map.lookup var env) of
+      Nothing -> var
+      Just val -> show val
+
+body :: P
+body = many1 $ noneOf ";"
+
+idChar :: Parsec String M Char
+idChar = letter <|> digit <|> char '_'
+
+idStr :: P
+idStr = many1 idChar
+
+header :: Parsec String M (String, [Int])
+header = do
+  env <- getState
+  char '(' >> sp
+  var <- (tryStr "int" >> sp >> idStr) <|> idStr
+  sp
+  firstStr <- sp >> char '=' >> sp >> many (noneOf " ;")
+  let firstVal = fromRight $ runParser eval env "" firstStr
+  condStr <- sp >> char ';' >> sp >> many (noneOf ";")
+  var' <- sp >> char ';' >> sp >> idStr
+  incr <- sp >> (string "++" <|> string "--")
+  sp >> char ')'
+  return $ (var, range (head incr) var env firstVal condStr)
+
+-- range '+' "i" empty 0 "i<3" = [0,1,2]
+range :: Char -> String -> M -> Int -> String -> [Int]
+range '+' var env firstVal cond =
+  takeWhile (testCond var env cond) [firstVal..]
+range '-' var env firstVal cond =
+  takeWhile (testCond var env cond) [firstVal, firstVal-1 ..]
+
+-- testCond "i" empty "i<3" 2 = True
+testCond :: String -> M -> String -> Int -> Bool
+testCond var env condStr val = fromRight $
+  runParser condParser (insert var val env) "" condStr
+
+condParser :: Parsec String M Bool
+condParser = do
+  x <- eval
+  op <- sp >> many1 (oneOf "!<>=")
+  y <- sp >> eval
+  return $ cond op x y
+
+cond :: String -> Int -> Int -> Bool
+cond ">" x y = x > y
+cond ">=" x y = x >= y
+cond "<" x y = x < y
+cond "<=" x y = x <= y
+cond "==" x y = x == y
+cond "!=" x y = x /= y
+
+-- eval (fromList [("i",3)]) "i+1" = 4
+eval :: Parsec String M Int
+eval = try termOpTerm <|> term
+
+-- term (fromList [("i",3)]) "i" = 3
+-- term (fromList [("i",3)]) "5" = 5
+term :: Parsec String M Int
+term = termInt <|> termVar
+
+termInt :: Parsec String M Int
+termInt = fmap read $ many1 digit
+
+termVar :: Parsec String M Int
+termVar = do
+  env <- getState
+  str <- idStr
+  return $ env ! str
+
+termOpTerm :: Parsec String M Int
+termOpTerm = do
+  x <- term
+  op <- sp >> oneOf "+-"
+  y <- sp >> term
+  return $ case op of
+    '+' -> x+y
+    '-' -> x-y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
