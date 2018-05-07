5664e3a02e7c4eab26e6bb8186983d3db14cc624
Dominic Prior <dominicprior@yahoo.co.uk>
2018-05-07 08:43:26 +0100

    Add braces

--- unroll.hs
+++ unroll.hs
@@ -5,6 +5,8 @@ import Data.Map
 type M = Data.Map.Map String Int
 type P = Parsec String M String
 sp = spaces
+
+fromRight (Left e) = error $ show e
 fromRight (Right x) = x
 
 tryStr :: String -> P
@@ -28,10 +30,11 @@ stmt = forLoop <|> simpleStmt
 
 simpleStmt :: P
 simpleStmt = do
-  s <- many space
+  s1 <- many space
   b <- many1 (noneOf ";")
   char ';'
-  return $ s ++ b ++ ";"
+  s2 <- many space
+  return $ s1 ++ b ++ ";" ++ s2
 
 k = t forLoop empty "for (i=0;i<3;i++) pr(i);"
 
@@ -40,14 +43,10 @@ forLoop = do
   tryStr "for" >> sp
   (var, a) <- header
   b <- body
-  char ';'
+  s <- many space
   env <- getState
   return $ concatMap
-      (f var env b)
-      --(
-      --(fromRight $ runParser program env "") .
-      --(subst' var env $ b ++ ";")
-      --)
+      (f var env $ b ++ s)
       a
 
 -- f "i" empty "pr(i)" 3 = "pr(3);"
@@ -82,15 +81,32 @@ substVar = do
       Just val -> show val
 
 body :: P
---body = many1 $ noneOf ";"
-body = fmap concat $
-         many1 (bodyTerm <|> fmap (:[]) (noneOf ";"))
+body = bodyBrace <|> bodyStmt
+
+bodyStmt :: P
+bodyStmt = do
+  b <- fmap concat $
+         many1 (bodyTerm <|> bodyTerm' <|> fmap (:[]) (noneOf ";"))
+  return $ b ++ ";"
+
+bodyBrace :: P
+bodyBrace = do
+  char '{'
+  s <- many (noneOf "}")
+  char '}'
+  return s
 
 bodyTerm :: P
 bodyTerm = do
   s <- char '(' >> many1 (noneOf ")")
   char ')'
   return $ "(" ++ s ++ ")"
+
+bodyTerm' :: P
+bodyTerm' = do
+  s <- char '{' >> many1 (noneOf "}")
+  char '}'
+  return $ "{" ++ s ++ "}"
 
 idChar :: Parsec String M Char
 idChar = letter <|> digit <|> char '_'
@@ -109,7 +125,7 @@ header = do
   condStr <- sp >> char ';' >> sp >> many (noneOf ";")
   var' <- sp >> char ';' >> sp >> idStr
   incr <- sp >> (string "++" <|> string "--")
-  sp >> char ')'
+  sp >> char ')' >> many newline
   return $ (var, range (head incr) var env firstVal condStr)
 
 -- range '+' "i" empty 0 "i<3" = [0,1,2]
